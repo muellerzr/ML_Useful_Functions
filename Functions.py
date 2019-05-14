7fe04d73fa2f6c1d5705ac3060f228f4ca4d09ab
Zachary Mueller <muellerzr@gmail.com>
2019-05-14 17:46:40 -0500

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -92,3 +92,37 @@ def findBestAlpha(data):
     with progress_disabled_ctx(learn) as learn:
       learn.fit_one_cycle(5)
     i += 1
+    
+    
+class SaveModelCallback(TrackerCallback):
+    "A `TrackerCallback` that saves the model when monitored quantity is best."
+    def __init__(self, learn:Learner, monitor:str='accuracy', mode:str='auto', every:str='improvement', name:str='bestmodel',):
+        super().__init__(learn, monitor=monitor, mode=mode)
+        self.every,self.name = every,name
+        if self.every not in ['improvement', 'epoch']:
+            warn(f'SaveModel every {self.every} is invalid, falling back to "improvement".')
+            self.every = 'improvement'
+
+                 
+    def jump_to_epoch(self, epoch:int)->None:
+        try: 
+            self.learn.load(f'{self.name}_{epoch-1}', purge=False)
+            print(f"Loaded {self.name}_{epoch-1}")
+        except: print(f'Model {self.name}_{epoch-1} not found.')
+
+    def on_epoch_end(self, epoch:int, **kwargs:Any)->None:
+        "Compare the value monitored to its best score and maybe save the model."
+        if self.every=="epoch": self.learn.save(f'{self.name}_{epoch}')
+        else: #every="improvement"
+            current = self.get_monitor_value()
+            if current is not None and self.operator(current, self.best):
+                #print(f'Better model found at epoch {epoch} with {self.monitor} value: {current}.')
+                self.best = current
+                self.learn.save(f'{self.name}')
+
+    def on_train_end(self, **kwargs):
+        "Load the best model."
+        if self.every=="improvement" and (self.learn.path/f'{self.learn.model_dir}/{self.name}.pth').is_file():
+            self.learn.load(f'{self.name}', purge=False)
+            current = self.get_monitor_value()
+            #print(float(current))
